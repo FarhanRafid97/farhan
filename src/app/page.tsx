aa32b0be552ba0dddf58a8ce633782dd8e6fe929
farhan rafid syauqi <farhanrafid97@gmail.com>
2023-08-26 03:26:58 +0700

    feat: add running text on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from '@/components/modules/Navbar/Navbar';
 import AboutMe from '@/components/modules/profile/AboutMe';
 import Hero from '@/components/modules/profile/Hero';
+import Journey from '@/components/modules/profile/Journey';
 import { ParallaxText } from '@/components/modules/profile/RunningText';
 
 export default function App() {
@@ -12,7 +13,8 @@ export default function App() {
         <Navbar />
         <Hero />
         <AboutMe />
-        <div className="flex flex-col gap-[-10px]">
+        <Journey />
+        <div className="flex flex-col gap-[-10px] mt-12">
           <ParallaxText baseVelocity={-5}>Farhan Rafid Syauqi</ParallaxText>
           <ParallaxText baseVelocity={-5}>Frontend Engineer</ParallaxText>
         </div>
