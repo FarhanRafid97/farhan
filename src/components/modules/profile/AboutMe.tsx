b1cd16a80c7f3049fe6b86f9adbf006a0cfef7c5
farhan rafid syauqi <farhanrafid97@gmail.com>
2023-08-26 03:25:48 +0700

    fix: fix aboutme section

--- src/components/modules/profile/AboutMe.tsx
+++ src/components/modules/profile/AboutMe.tsx
@@ -4,20 +4,19 @@ import { FirstAsset, SeccondAsset } from './assets';
 const AboutMe = () => {
   return (
     <section className="section-container">
-      <div className="w-full h-screen flex flex-col p-4">
+      <div className="w-full h-screen flex flex-col">
         <div className="flex-1  h-full">
           <div className="w-[40px] flex-1">
             <SeccondAsset />
           </div>
           <div className="flex-2 w-full md:w-1/2 mt-8 py-4 padding-left-layout ">
             <h1 className="text-[47px] lg:text-[57px] text-primary text-center md:text-start font-bold mb-4">
-              Backend Engineering
+              Backend
             </h1>
             <p className="text-ascent text-sm lg:text-base text-justify">
-              Backend engineer with a passion for building robust systems. Proficient in Golang and
-              Node.js, I specialize in developing microservices with a focus on efficient
-              communication through gRPC. My expertise extends to SQL and Redis, enabling me to
-              design and optimize databases for high-performance applications.
+              Proficient in Golang and Node.js, I specialize in developing microservices with a
+              focus on efficient communication through gRPC. My expertise extends to SQL and Redis,
+              enabling me to design and optimize databases for high-performance applications.
             </p>
           </div>
         </div>
@@ -27,7 +26,7 @@ const AboutMe = () => {
           </div>
           <div className="flex-2 w-full md:w-1/2 mt-3 py-4 padding-right-layout">
             <h1 className="text-[47px] lg:text-[57px] text-primary text-center md:text-end lg:text-start font-bold mb-4">
-              Frontend Engineering
+              Frontend
             </h1>
             <p className="text-ascent text-justify">
               Experienced frontend engineer specializing in Next.js and TypeScript environments.
